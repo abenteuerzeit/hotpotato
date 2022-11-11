dd6bdcf84564ce5d3f58e21696b390b112f2b4d4
abenteuerzeit <amroz.muzyka@gmail.com>
2022-11-11 18:05:45 +0100

    refactor: Replace magic string with variables

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const server = http.createServer((req, res) => {
 ///////////////////////////////////////////////
 
 // TODO: Create the WebSocket Server (ws) using the HTTP server
-const option = {server: 'localhost:8080'};
+const option = {server: server};
 const wsServer = new WebSocket.Server(option);
 wsServer.on('connection', (socket) => {
   console.log('A new client has joined the server');
